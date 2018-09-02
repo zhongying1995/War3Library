c3d24c167e2f1bec5416509c468f7b1a79d73c53
zhongying1995 <1424249327@qq.com>
2018-09-02 22:04:56 +0800

    开始检查kernel层代码

--- _init.lua
+++ _init.lua
@@ -1,6 +1,9 @@
 print('------------------')
 print('即将开始加载底层代码')
-print('------------------')
+print('vvvvvvvvvvvvvvvvvvvv')
 require 'kernel._init'
-require 'libraries._init'
-require 'framwork._init'
+-- require 'libraries._init'
+-- require 'framwork._init'
+print('^^^^^^^^^^^^^^^^^^^^')
+print('加载底层代码完成')
+print('------------------')

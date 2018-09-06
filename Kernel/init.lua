5e0f96d9f749c97979e5ae4a2052bfe2b8888fa4
zhongying1995 <1424249327@qq.com>
2018-09-06 23:12:09 +0800

    当访问不存在的值时，抛出error

--- Kernel/init.lua
+++ Kernel/init.lua
@@ -1,4 +1,10 @@
 --ac:全局表，用于方便使用某些功能
 ac = {}
+setmetatable(ac, {__index = function ( self, key )
+    Log.error('尝试在ac中访问不存在的值:' .. key)
+end})
 --Rount：全局表，用于返回核心层的模块、framework层的模块，而不是通过require来获取
 Rount = {}
+setmetatable(Rount, {__index = function ( self, key )
+    Log.error('尝试在Rount中访问不存在的值:' .. key)
+end})

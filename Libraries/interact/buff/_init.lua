b567121fe55034e78fc88ece561eac0d51eee971
zhongying1995 <1424249327@qq.com>
2018-10-13 16:55:41 +0800

    交互层，增加内置的buff

---
+++ Libraries/interact/buff/_init.lua
@@ -0,0 +1,6 @@
+--暂时别加这个吧
+-- require 'libraries.interact.buff.template'
+require 'libraries.interact.buff.减攻速'
+require 'libraries.interact.buff.减速'
+require 'libraries.interact.buff.缩放'
+require 'libraries.interact.buff.高度'

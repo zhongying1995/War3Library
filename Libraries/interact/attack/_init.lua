6e70f835ce7f19b0afdfc983d0bd9f0a33a82a88
zhongying1995 <1424249327@qq.com>
2018-11-17 16:14:39 +0800

    增加溅射模块

--- Libraries/interact/attack/_init.lua
+++ Libraries/interact/attack/_init.lua
@@ -1,3 +1,8 @@
-local Crit = require 'war3library.libraries.interact.attack.crit'
+--这些写法不正确，应该封装对应的对象，然后添加给单位，下次大重构时，再那样把
 
+local Crit = require 'war3library.libraries.interact.attack.crit'
+local Cleave = require 'war3library.libraries.interact.attack.cleave'
+
+--初始化
 Crit.init()
+Cleave.init()

7f509883582f1d61a54432acba01c9ac3cbd39b6
zhongying1995 <1424249327@qq.com>
2018-11-18 23:34:17 +0800

    治疗模块，增加持续治疗时间功能

--- Libraries/types/heal.lua
+++ Libraries/types/heal.lua
@@ -27,6 +27,21 @@ mt.heal = 0
 --关联技能
 mt.skill = nil
 
+--治疗的频率
+mt.pulse = 0.1
+
+--治疗持续时间
+mt.duration = nil
+
+--特效,默认显示
+mt.effect = [[Abilities\Spells\Items\HealingSalve\HealingSalveTarget.mdl]]
+
+--单位位置
+mt.ref = 'origin'
+
+--默认显示文字
+mt.is_show = true
+
 --创建漂浮文字
 local function text(heal)
 	
@@ -47,8 +62,46 @@ local function text(heal)
 	
 end
 
+function Heal:remove(  )
+	if self._removed then
+		return
+	end
+	if self.on_remove then
+		self:on_remove()
+	end
+	if self.duration_timer then
+		self.duration_timer:remove()
+	end
+end
+
+--立刻治疗一次
+function mt:heal_start()
+	self.source:event_notify('单位-给予治疗开始', self)
+	if self.target:event_dispatch('单位-受到治疗开始', self) then
+		return self
+	end
+	
+	if self.heal < 0 then
+		self.heal = 0
+	end
+	--进行治疗
+	self.target:add_life(self.heal)
+	
+	if self.effect then
+		self.target:add_effect(self.effect, self.ref):remove()
+	end
+	
+	--创建漂浮文字
+	if self.is_show then
+		text(self)
+	end
+	
+	self.source:event_notify('单位-给予治疗效果', self)
+	self.target:event_notify('单位-受到治疗效果', self)
+end
+
 --创建治疗
-function Heal:__call(heal)
+function Heal:new(heal)
 	if not heal.target or heal.heal == 0 then
 		return
 	end
@@ -60,23 +113,22 @@ function Heal:__call(heal)
 	
 	setmetatable(heal, self)
 
-	heal.source:event_notify('单位-给予治疗开始', heal)
-	if heal.target:event_dispatch('单位-受到治疗开始', heal) then
-		return heal
+
+	if heal.duration then
+		heal.elapsed_time = 0
+		heal.heal = heal.heal * heal.pulse / heal.duration
+		heal.duration_timer = ac.loop(heal.pulse*1000, function()
+			if heal.elapsed_time > heal.duration then
+				heal.duration_timer:remove()
+				return
+			end
+			heal:heal_start()
+			heal.elapsed_time = heal.elapsed_time + heal.pulse
+		end)
+		heal.duration_timer:on_timer()
+	else
+		heal:heal_start()
 	end
-
-	if heal.heal < 0 then
-		heal.heal = 0
-	end
-	
-	--进行治疗
-	heal.target:add_life(heal.heal)
-
-	--创建漂浮文字
-	text(heal)
-
-	heal.source:event_notify('单位-给予治疗效果', heal)
-	heal.target:event_notify('单位-受到治疗效果', heal)
 
 	return heal
 end
@@ -87,7 +139,7 @@ function Unit.__index:heal(data)
 	if not data.target then
 		data.target = self
 	end
-	return Heal(data)
+	return Heal:new(data)
 end
 
 return Heal

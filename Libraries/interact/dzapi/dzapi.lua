48826a52762f8cd4d51d1b057a2e186302bbad32
zhongying1995 <1424249327@qq.com>
2018-12-13 22:10:07 +0800

    dzapi，改一下命名

--- Libraries/interact/dzapi/dzapi.lua
+++ Libraries/interact/dzapi/dzapi.lua
@@ -15,7 +15,7 @@ local Dzapi = {}
 --  key：键
 --  val：值
 --  @是否保持成功
-function Dzapi.dz_set_server_value( player, key, val )
+function Dzapi.set_server_value( player, key, val )
     local key = tostring(key)
     local val = tostring(val)
     save_player(Interactive, 0, 1, player.handle)
@@ -29,7 +29,7 @@ end
 --获取玩家服务器信息
 --  玩家
 --  键
-function Dzapi.dz_get_server_value( player, key )
+function Dzapi.get_server_value( player, key )
     local key = tostring(key)
     save_player(Interactive, 0, 1, player.handle)
     save_str(Interactive, 0, 2, key)
@@ -41,8 +41,11 @@ end
 --是否从rpg大厅来的
 --  @本地yd测试时返回 true
 --  @平台自主c房间时返回 false
-function Dzapi.dz_is_rpg_robby(  )
+function Dzapi.is_rpg_robby(  )
     execute_func("dz_is_rpg_robby")
     local robby = load_boolean(Interactive, 0, 0)
     return robby
 end
+
+
+return Dzapi
